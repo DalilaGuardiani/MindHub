08e423123ab3f3392c0a36afb0d7fc6f6d538c53
Matteo Santarelli <matteosantarelli@gmail.com>
2026-06-02 12:08:46 +0200

    Avatar profilo backend

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -32,7 +32,7 @@ router.get("/:id", async (req, res) => {
 
     const { data: user, error: userError } = await supabase
       .from("users")
-      .select("id, username, email, created_at")
+      .select("id, username, email, profile_image, created_at")
       .eq("id", id)
       .maybeSingle();
 
@@ -103,5 +103,88 @@ router.get("/:id/scores", async (req, res) => {
     });
   }
 });
+
+router.put("/:id/avatar", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { profileImage } = req.body;
+
+    const allowedAvatars= [
+      "alien.png",
+      "cyborg.png",
+      "girl.png",
+      "hacker.png",
+      "metaverse.png",
+      "ninja.png",
+      "octopus.png",
+      "pirate.png",
+      "robot.png",
+      "samurai.png"
+    ];
+    if (!allowedAvatars.includes(profileImage)) {
+      return res.status(400).json({
+        success: false,
+        message: "Immagine del profilo non valida"
+      });
+    }
+
+
+
+    const { data: user, error: userError } = await supabase
+      .from("users")
+      .update({ profile_image: profileImage })
+      .eq("id", id)
+      .select("id, profile_image")
+      .maybeSingle();
+
+    if (userError || !user) {
+      return res.status(404).json({
+        success: false,
+        message: "Utente non trovato"
+      });
+    }
+
+    return res.json({
+      success: true,
+      message: "Avatar aggiornato con successo",
+      user: user
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Errore interno del server"
+    });
+  }
+});
+
+router.delete("/:id/avatar", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { data: user, error: userError } = await supabase
+      .from("users")
+      .update({ profile_image: null })
+      .eq("id", id)
+      .select("id, username, email, profile_image")
+      .maybeSingle();
+    if (userError || !user) {
+      return res.status(404).json({
+        success: false,
+        message: "Utente non trovato"
+      });
+    }
+  return res.json({
+    success: true,
+    message: "Avatar rimosso con successo",
+    user: user
+  });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Errore interno del server"
+    });
+  }
+}
+);
 
 module.exports = router;
